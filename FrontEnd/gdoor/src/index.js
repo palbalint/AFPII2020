fb9df56a8a0cc4d06d1b99eb9cfcdfc0c5a488df
dancsviktor99 <55428134+dancsviktor99@users.noreply.github.com>
2020-04-08 15:22:11 +0200

    Login-Logout-Register felület
    Folyamatban van, egy -két hiba jelentkezik, debug még ma megfog történni.

--- FrontEnd/gdoor/src/index.js
+++ FrontEnd/gdoor/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+window.axios=axios;
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +17,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker();
+
+
+
